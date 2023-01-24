4ca6cc7bad9469a865181ce0a56b87a66642f8e5
Andrew <andrew.suponev@gmail.com>
2023-01-24 08:52:21 +0300

    fix: correct format data

--- src/services/createRandomPerson.js
+++ src/services/createRandomPerson.js
@@ -2,23 +2,14 @@ import { faker } from '@faker-js/faker';
 
 const createRandomPerson = (i) => {
 
-  let personId = `
-      ${faker.random.alpha({ count: 2, casing: 'upper' })}
-      ${faker.datatype.number({ min: 1000000, max: 9999999 })}`;
+  let personId = `${faker.random.alpha({ count: 2, casing: 'upper' })} ${faker.datatype.number({ min: 1000000, max: 9999999 })}`;
 
   let name = faker.name.fullName();
 
-  let address = `
-    ${faker.address.state()},
-    ${faker.address.city()},
-    ${faker.address.street()},
-    ${faker.address.buildingNumber()},
-    ${faker.address.secondaryAddress()}`
+  let address = `${faker.address.state()}, ${faker.address.city()}, ${faker.address.street()}, ${faker.address.buildingNumber()}, ${faker.address.secondaryAddress()}`
 
   const basePhone = faker.phone.number();
-  let phone = basePhone.indexOf('x') === -1 ? 
-        basePhone
-        : basePhone.slice(0, basePhone.indexOf('x'));
+  let phone = basePhone.indexOf('x') === -1 ? basePhone : basePhone.slice(0, basePhone.indexOf('x'));
 
     return {
       number: i,
